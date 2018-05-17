e53c152b958a1ff9c14db7479c752acd9ec021f2
Support <support@agilelabs.com>
2018-05-17 07:57:52 -0700

    update

--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 
+import Key from './Key';
+
 class Game extends Component {
   render() {
     return (
       <div className="game">
-        <div className="help">
-          Pick 4 numbers that sum to the target in 15 seconds
-        </div>
+        <div className="help">Pick 4 numbers that sum to the target in 15 seconds</div>
         <div className="target">42</div>
         <div className="challenge-numbers">
-          <div className="number">8</div>
-          <div className="number">5</div>
-          <div className="number">12</div>
-          <div className="number">13</div>
-          <div className="number">5</div>
-          <div className="number">16</div>
+          <Key value={8} />
+          <Key value={5} />
+          <Key value={12} />
+          <Key value={13} />
+          <Key value={16} />
+          <Key value={17} />
         </div>
         <div className="footer">
           <div className="timer-value">15</div>
